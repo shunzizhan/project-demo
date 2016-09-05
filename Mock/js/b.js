f242847cebd9d1e9820b806e7010629ae8f8a368
shunzizhan <shunzizhan@163.com>
2016-09-05 20:09:54 +0800

    Mock

---
+++ Mock/js/b.js
@@ -0,0 +1,23 @@
+// __inline('a.js');
+
+var getTestData = function(){
+  $.ajax({
+    url: 'api/user',
+    type: 'GET',
+    dataType: 'json',
+    // data: {param1: 'value1'},
+  })
+  .done(function(res) {
+    console.log("success");
+    console.log(res.data.uname+">>"+res.data.uid);
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+  
+}
+
+// getTestData();
