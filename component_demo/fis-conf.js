8bdd38a8448bbc66c78a0bb541147d37b3e08c0a
shunzizhan <shunzizhan@163.com>
2016-10-10 15:00:47 +0800

    模块化模板

--- component_demo/fis-conf.js
+++ component_demo/fis-conf.js
@@ -1,42 +1,57 @@
 
 //项目配置，将name、version独立配置，统管全局
 fis.set('name', 'proj');
-fis.set('version', '1.0.0');
+fis.set('version', '1.0.1');
 // 测试环境
 fis.set('domain_test', ''); //开发环境静态资源
 // 预发布环境
-fis.set('domain_pre', 'http://preuc.fdc.com.cn');
+fis.set('domain_pre', '');
 // 线上环境
-// fis.set('domain_build', 'http://img3.fdc.com.cn/usercenter');
 fis.set('domain_build', '');
+
 // 过滤指定的文件类型
 fis.set('project.files', [
   '*.css',
   '*.js',
-  '*.html'
+  '*.html',
+  '*.{png,jpg,gif}'
 ]);
 
 fis
-  .match(/^\/libs\/(.*)$/gi, {
-    release: '/${name}_${version}/libs/$1'
+  // 静态资源，包含第三方库文件，通用的js/css/img
+  .match(/^\/libs\/(.*)$/i, {
+    release: '/public/libs/$1'
   })
-  .match(/^\/UI\/(common).less$/gi, {
+  .match(/^\/public\/img\/(.*)$/i, {
+    release: '/public/images/$1'
+  })
+  .match(/^\/public\/less\/(\w+)\.less$/i, {
+    parser: fis.plugin('less'),
+    rExt: '.css',
+    release: '/public/css/$1'
+  })
+  .match(/^\/public\/js\/(.*)$/i, {
+    release: '/public/js/$1'
+  })
+  // 业务逻辑
+  // *********less******************
+  .match(/^\/pages\/\w+\/(\w+)\.less$/i, {
     parser: fis.plugin('less'),
     rExt: '.css',
     release: '/${name}_${version}/css/$1'
   })
-  .match(/^\/pages\/\w+\/(\w+)\.less$/gi, {
-    parser: fis.plugin('less'),
-    rExt: '.css',
-    release: '/${name}_${version}/css/$1'
-  })
-  .match(/^\/pages\/\w+\/(\w+)\.js$/gi, {
+  // *********js******************
+  .match(/^\/pages\/\w+\/(\w+)\.js$/i, {
     release: '/${name}_${version}/js/$1'
   })
-  .match(/^\/pages\/\w+\/(\w+)\.html$/gi, {
+  // *********html******************
+  .match(/^\/pages\/\w+\/(\w+)\.html$/i, {
     release: '/${name}_${version}/$1'
   })
-  
+  // *********图片*********
+  .match(/^\/pages\/(\w+)\/img\/(.*)$/i, {
+    release: '/${name}_${version}/images/$1/$2'
+  })
 
 // 测试开发
 fis.media('test')
@@ -63,9 +78,9 @@ fis.media('pre')
 
 // 线上
 fis.media('build')
-    .match('*', {
-      domain: "${domain_build}"
-    })
+    // .match('*', {
+    //   domain: "${domain_build}"
+    // })
     // .match('*.html', {
     //   deploy: fis.plugin('http-push', {
     //     receiver: 'http://192.168.1.9:8999/receiver',
